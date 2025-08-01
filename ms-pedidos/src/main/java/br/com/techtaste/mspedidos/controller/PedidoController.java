bd945166f51f123344031bd317b0686c7343294d
Philippe Maia <philippe.m.maia@hotmail.com>
2025-08-01 01:05:42 -0300

    Implementando o API Gateway

--- ms-pedidos/src/main/java/br/com/techtaste/mspedidos/controller/PedidoController.java
+++ ms-pedidos/src/main/java/br/com/techtaste/mspedidos/controller/PedidoController.java
@@ -4,6 +4,7 @@ import br.com.techtaste.mspedidos.dto.PedidoRequestDto;
 import br.com.techtaste.mspedidos.dto.PedidoResponseDto;
 import br.com.techtaste.mspedidos.service.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,5 +26,11 @@ public class PedidoController {
     @GetMapping
     public List<PedidoResponseDto> obterTodos() {
         return service.obterTodos();
+    }
+
+    @GetMapping("/response")
+    public String obterPorta(@Value("${local.server.port}") String porta) {
+        return String.format("Resposta vinda da porta %s", porta);
+
     }
 }
